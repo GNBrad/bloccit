1f3f0a67907ee59992d90e036b044af88f23a3f1
Brad Z <brad@greennebula.biz>
2013-07-24 16:53:11 +0200

    Latest commit to look for errors in code

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar
-  has_many :posts
+  has_many :posts, :comments
 
   before_create :set_member
   # attr_accessible :title, :body
